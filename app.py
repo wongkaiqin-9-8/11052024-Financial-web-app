8280a33d679e82b9f6decca22d640abab47947cd
Teoh TeikToe <teohteiktoe@gmail.com>
2024-05-11 14:58:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,5 +11,14 @@ def main():
     r = request.form.get("q")
     return(render_template("main.html",r=r))
 
+@app.route("/prediction",methods=["GET","POST"])
+def prediction():
+    return(render_template("prediction.html"))
+
+@app.route("/dbs_price",methods=["GET","POST"])
+def dbs_price():
+    q = float(request.form.get("q"))
+    return(render_template("dbs_price.html",r=(q*-50.6)+90.2))
+
 if __name__ == "__main__":
     app.run()
