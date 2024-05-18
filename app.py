9cc2f4b89d455440534dcfc6dffc6a9762ec5590
wongkaiqin-9-8 <wongkaiqin98@gmail.com>
2024-05-18 14:33:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,69 @@
 from flask import Flask, render_template, request
+import google.generativeai as palm
+import replicate
+import os
 
-app = Flask(__name__)
+flag = 1
+name = ""
 
-@app.route("/",methods=["GET","POST"])
+makersuite_api = os.getenv("MAKERSUITE_API_TOKEN")
+palm.configure(api_key=makersuite_api)
+
+model = {'model' :"models/chat-bison-001"}
+app = Flask(__name__) #to register the web as yours
+
+@app.route("/",methods=["GET","POST"]) #get from frontend, post to frontend
 def index():
     return(render_template("index.html"))
 
 @app.route("/main",methods=["GET","POST"])
 def main():
-    r = request.form.get("q")
-    return(render_template("main.html",r=r))
+    global flag, name
+    if flag == 1: #to ensure that only have to type in the name once
+        name = request.form.get("q") #backend
+        flag = 0
+    return(render_template("main.html",r=name)) #r frontend to receive from backend
 
 @app.route("/prediction",methods=["GET","POST"])
 def prediction():
-    return(render_template("prediction.html"))
+    return(render_template("prediction.html")) #r frontend to receive from backend
 
 @app.route("/dbs_price",methods=["GET","POST"])
 def dbs_price():
-    q = float(request.form.get("q"))
+    q = float(request.form.get("q")) #backend
     return(render_template("dbs_price.html",r=(q*-50.6)+90.2))
 
-if __name__ == "__main__":
+@app.route("/generate_text",methods=["GET","POST"]) #get from backend, post to frontend
+def generate_text():
+    return(render_template("generate_text.html"))
+
+@app.route("/text_result_makersuite",methods=["GET","POST"])
+def text_result_makersuite():
+    q = request.form.get("q") #backend
+    r = palm.chat(**model, messages=q)
+    return(render_template("text_result_makersuite.html", r=r.last))
+
+
+
+@app.route("/generate_image",methods=["GET","POST"]) #get from backend, post to frontend
+def generate_image():
+    return(render_template("generate_image.html"))
+
+@app.route("/image_result",methods=["GET","POST"])
+def image_result():
+    q = request.form.get("q") #backend
+    r = replicate.run("stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+      input = {"prompt": q}
+    )
+    return(render_template("image_result.html", r=r[0]))
+
+
+@app.route("/end",methods=["GET","POST"]) #get from backend, post to frontend
+def end():
+    global flag
+    flag = 1
+    return(render_template("index.html"))
+
+
+if __name__ == "__main__": #ensure that its u again
     app.run()
